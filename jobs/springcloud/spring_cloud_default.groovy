345a8447c488a96c8d0b2167c6371e7ac3d898ec
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-04-22 13:24:12 +0200

    Initial commit

---
+++ jobs/springcloud/spring_cloud_default.groovy
@@ -0,0 +1,45 @@
+import javaposse.jobdsl.dsl.DslFactory
+
+DslFactory dsl = this
+
+def springCloudBuildUrl = 'https://github.com/spring-cloud/spring-cloud-build'
+def springBootVersion = '1.4.0.BUILD-SNAPSHOT'
+def gistUrl = 'https://gist.githubusercontent.com/marcingrzejszczak/e63d4985f2a12d51af3310be51b2caa2/raw/c741bfea548b2b99fc242d743b1270301eab5167/replace_parent_version_in_pom.groovy'
+def groovyLocation = '/opt/groovy/2.4.5/bin/groovy'
+def projects = ['spring-cloud-sleuth', 'spring-cloud-netflix', 'spring-cloud-zookeeper']
+
+projects.eachWithIndex { String projectName, Integer index ->
+	dsl.job("${projectName}-compatibility-check") {
+		triggers {
+			cron everyDayAt(index)
+		}
+		scm {
+			git("https://github.com/spring-cloud/$it")
+		}
+		steps {
+			shell("""
+					echo "Cloning spring-cloud-build"
+					git clone $springCloudBuildUrl
+					echo "Downloading and running script to change parent version"
+					wget $gistUrl --no-check-certificate
+					$groovyLocation replace_parent_version_in_pom.groovy -p "spring-cloud-build/pom.xml" -v "$springBootVersion"
+					""")
+			shell('''
+					echo "Installing built version with different parent"
+					./spring-cloud-build/mvnw clean install
+					''')
+			shell("""
+					echo -e "Printing the list of dependencies for [$projectName]"
+					./mvnw dependency:tree
+					""")
+			shell('''
+					echo -e "Running the tests"
+					./mvnw clean verify -fae
+					''')
+		}
+	}
+}
+
+String everyDayAt(int offset, int startingHour = 5) {
+	return "0 0 ${startingHour + offset} 1/1 * ? *"
+}
---
+++ src/test/groovy/io/springframework/cloud/JobScriptsSpec.groovy
@@ -0,0 +1,38 @@
+package io.springframework.cloud
+
+import groovy.io.FileType
+import javaposse.jobdsl.dsl.DslScriptLoader
+import javaposse.jobdsl.dsl.MemoryJobManagement
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Tests that all dsl scripts in the jobs directory will compile.
+ */
+class JobScriptsSpec extends Specification {
+
+    @Unroll
+    void 'test script #file.name'(File file) {
+        given:
+            MemoryJobManagement jm = new MemoryJobManagement()
+
+        when:
+            DslScriptLoader.runDslEngine(file.text, jm)
+
+        then:
+            noExceptionThrown()
+
+        where:
+            file << jobFiles
+    }
+
+    static List<File> getJobFiles() {
+        List<File> files = []
+        new File('jobs').eachFileRecurse(FileType.FILES) {
+            files << it
+        }
+        return files
+    }
+
+}
+
