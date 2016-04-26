4f73b9197b4e4c1d58543193058743a0c7a9e8d5
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-04-26 17:29:17 +0200

    Changing the way script is ran

--- src/main/groovy/io/springframework/cloud/e2e/CloudFoundryEndToEndBuildMaker.groovy
+++ src/main/groovy/io/springframework/cloud/e2e/CloudFoundryEndToEndBuildMaker.groovy
@@ -72,7 +72,7 @@ class CloudFoundryEndToEndBuildMaker implements NotificationTrait, PublisherTrai
 						cf login -u ${cfUsername()} -p ${cfPassword()} -o FrameworksAndRuntimes -s ${cfSpace()}
 
 						echo "Running script CF"
-						sh -e ${script}
+						./${script}
 					""")
 			}
 			configure {
