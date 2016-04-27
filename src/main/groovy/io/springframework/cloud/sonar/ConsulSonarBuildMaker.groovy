a5e9e9fbf419bd2a566a70a591d000bf5c2f4297
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-04-27 16:31:36 +0200

    Refactoring

--- src/main/groovy/io/springframework/cloud/sonar/ConsulSonarBuildMaker.groovy
+++ src/main/groovy/io/springframework/cloud/sonar/ConsulSonarBuildMaker.groovy
@@ -2,11 +2,11 @@ package io.springframework.cloud.sonar
 
 import io.springframework.cloud.common.ConsulTrait
 import javaposse.jobdsl.dsl.DslFactory
+
 /**
  * @author Marcin Grzejszczak
  */
 class ConsulSonarBuildMaker extends SonarBuildMaker implements ConsulTrait {
-	private  static final String ONCE_PER_DAY = "H H * * *"
 
 	ConsulSonarBuildMaker(DslFactory dsl) {
 		super(dsl)
