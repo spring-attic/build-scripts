fb3a19cbb6387c76736877c32220e25af0793036
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-04 19:55:11 +0200

    Added test check for CI

--- src/main/groovy/io/springframework/cloud/ci/SpringCloudDeployBuildMaker.groovy
+++ src/main/groovy/io/springframework/cloud/ci/SpringCloudDeployBuildMaker.groovy
@@ -2,19 +2,20 @@ package io.springframework.cloud.ci
 
 import io.springframework.common.JdkConfig
 import io.springframework.common.Notification
+import io.springframework.common.Publisher
 import javaposse.jobdsl.dsl.DslFactory
 
 /**
  * @author Marcin Grzejszczak
  */
-class SpringCloudDeployBuildMaker implements Notification, JdkConfig {
+class SpringCloudDeployBuildMaker implements Notification, JdkConfig, Publisher {
 	private final DslFactory dsl
 
 	SpringCloudDeployBuildMaker(DslFactory dsl) {
 		this.dsl = dsl
 	}
 
-	void deploy(String project) {
+	void deploy(String project, boolean checkTests = true) {
 		dsl.job("$project-ci") {
 			triggers {
 				githubPush()
@@ -38,9 +39,13 @@ class SpringCloudDeployBuildMaker implements Notification, JdkConfig {
 					./mvnw deploy -nsu -Dmaven.test.redirectTestOutputToFile=true
 					''')
 			}
-
 			configure {
 				appendSlackNotificationForSpringCloud(it as Node)
+			}
+			if (checkTests) {
+				publishers {
+					archiveJunit mavenJunitResults()
+				}
 			}
 		}
 	}
