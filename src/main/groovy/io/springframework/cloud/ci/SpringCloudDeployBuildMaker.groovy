c907eef687d78dc646fb287b58e842094445f0f5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-05-04 23:32:57 +0200

    Resetting repo before deploying snapshots

--- src/main/groovy/io/springframework/cloud/ci/SpringCloudDeployBuildMaker.groovy
+++ src/main/groovy/io/springframework/cloud/ci/SpringCloudDeployBuildMaker.groovy
@@ -36,7 +36,7 @@ class SpringCloudDeployBuildMaker implements Notification, JdkConfig, Publisher 
 					./docs/src/main/asciidoc/ghpages.sh
 					''')
 				shell('''
-					git stash && git checkout master
+					git reset --hard && git checkout master
 					./mvnw clean deploy -nsu -Dmaven.test.redirectTestOutputToFile=true
 					''')
 			}
