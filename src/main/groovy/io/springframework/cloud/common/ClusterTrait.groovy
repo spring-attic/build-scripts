feb8b08427f7bbc2b007b92f74037b4b6a15b130
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-05 12:53:57 +0200

    Another approach to ETCD

--- src/main/groovy/io/springframework/cloud/common/ClusterTrait.groovy
+++ src/main/groovy/io/springframework/cloud/common/ClusterTrait.groovy
@@ -7,17 +7,20 @@ trait ClusterTrait {
 
 	String preClusterShell() {
 		return '''
-					echo "Killing all docker instances"
-					docker kill $(docker ps -q) || echo "No containers are running"
-
-					echo "Running etcd"
-					docker run -d --name etcd quay.io/coreos/etcd:v2.3.3
+					mkdir -p target
+					cd target
+					curl -L  https://github.com/coreos/etcd/releases/download/v2.3.3/etcd-v2.3.3-linux-amd64.tar.gz -o etcd-v2.3.3-linux-amd64.tar.gz
+					tar xzvf etcd-v2.3.3-linux-amd64.tar.gz
+					cd etcd-v2.3.3-linux-amd64
+					nohup ./etcd &
+					cd ../..
 				'''
 	}
 
 	String postClusterShell() {
 		return '''
-					echo "Killing all docker instances" && docker kill $(docker ps -q) || echo "No containers are running"
+					pkill etcd && echo "Killed ETCD" || echo "No ETCD process is running"
+					rm -rf target && echo "Removed ETCD target" || echo "NO ETCD target"
 					'''
 	}
 }
