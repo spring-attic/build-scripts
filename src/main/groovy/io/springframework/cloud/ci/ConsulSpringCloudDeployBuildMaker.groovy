7fc83b0ac21e4b4890fdf74c27f476fc7ae5ece2
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-05-06 19:49:13 +0200

    Added a fix for Consul

--- src/main/groovy/io/springframework/cloud/ci/ConsulSpringCloudDeployBuildMaker.groovy
+++ src/main/groovy/io/springframework/cloud/ci/ConsulSpringCloudDeployBuildMaker.groovy
@@ -40,11 +40,10 @@ class ConsulSpringCloudDeployBuildMaker implements Notification, JdkConfig, Publ
 				shell('''
 						./mvnw install -P docs -q -U -DskipTests=true -Dmaven.test.redirectTestOutputToFile=true
 						./docs/src/main/asciidoc/ghpages.sh
-						git reset --hard && git checkout master
+						git reset --hard && git checkout master && git pull origin master
 					''')
 				shell preConsulShell()
 				shell("""
-						git reset --hard && git checkout master && git pull origin master &&
 						./mvnw clean deploy -nsu -Dmaven.test.redirectTestOutputToFile=true || ${postConsulShell()}
 					""")
 				shell postConsulShell()
