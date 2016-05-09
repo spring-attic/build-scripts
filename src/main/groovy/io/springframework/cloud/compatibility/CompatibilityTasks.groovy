5639c9edeaa9cd6280711c5150087de796fa602e
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-05-09 20:43:01 +0200

    Generating compatibility builds for spring boot 1.3.5-snap

--- src/main/groovy/io/springframework/cloud/compatibility/CompatibilityTasks.groovy
+++ src/main/groovy/io/springframework/cloud/compatibility/CompatibilityTasks.groovy
@@ -9,21 +9,23 @@ import javaposse.jobdsl.dsl.helpers.step.StepContext
 @PackageScope
 abstract class CompatibilityTasks {
 
+	private static final String springBootVersion = '1.3.5.BUILD-SNAPSHOT'
+
 	Closure defaultSteps() {
 		return buildStep {
 			shell("""
 					echo -e "Printing the list of dependencies"
-					./mvnw dependency:tree -U -Dspring-boot.version=1.4.0.BUILD-SNAPSHOT
+					./mvnw dependency:tree -U -Dspring-boot.version=${springBootVersion}
 					""")
 			shell runTests()
 		}
 	}
 
 	protected String runTests() {
-		return '''
+		return """
 					echo -e "Running the tests"
 					./mvnw clean install
-					./mvnw verify -U -fae -Dspring-boot.version=1.4.0.BUILD-SNAPSHOT'''
+					./mvnw verify -U -fae -Dspring-boot.version=${springBootVersion}"""
 	}
 
 	private Closure buildStep(@DelegatesTo(StepContext) Closure buildSteps) {
