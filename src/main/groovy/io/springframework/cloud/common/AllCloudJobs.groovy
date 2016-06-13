47c51e471569619e40228cecb1a57e7f8fd1935d
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-13 16:00:14 +0200

    Added spring cloud starters

--- src/main/groovy/io/springframework/cloud/common/AllCloudJobs.groovy
+++ src/main/groovy/io/springframework/cloud/common/AllCloudJobs.groovy
@@ -9,6 +9,7 @@ import groovy.transform.CompileStatic
 class AllCloudJobs {
 	public static final List<String> ALL_JOBS = ['spring-cloud-sleuth', 'spring-cloud-netflix', 'spring-cloud-zookeeper', 'spring-cloud-consul',
 												 'spring-cloud-bus', 'spring-cloud-commons', 'spring-cloud-config', 'spring-cloud-security',
-												 'spring-cloud-cloudfoundry', 'spring-cloud-aws', 'spring-cloud-build', 'spring-cloud-cluster']
+												 'spring-cloud-cloudfoundry', 'spring-cloud-aws', 'spring-cloud-build', 'spring-cloud-cluster',
+												 'spring-cloud-starters']
 	public static final List<String> ALL_SAMPLES_JOBS = ['tests']
 }
