74a538810274227490f56a5bb45707838648a2ff
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-30 15:16:44 +0200

    Disabled the build

--- jobs/springio/spring_io.groovy
+++ jobs/springio/spring_io.groovy
@@ -1,6 +1,5 @@
 package springio
 
-import io.springframework.springio.ci.SpringStarterDeployBuildMaker
 import javaposse.jobdsl.dsl.DslFactory
 
 import static io.springframework.springio.common.AllSpringIoJobs.ALL_JOBS
@@ -11,5 +10,5 @@ println "Projects with tests $ALL_JOBS"
 
 // CI
 ALL_JOBS.each {
-	new SpringStarterDeployBuildMaker(dsl).deploy(it)
+	//new SpringStarterDeployBuildMaker(dsl).deploy(it)
 }
