563e19fc482ff732a761d02a7f41aeb01a7524b1
Soby Chacko <schacko@pivotal.io>
2016-10-10 18:15:33 -0400

    Add app generation for scst app starters

--- src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStartersBuildMaker.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStartersBuildMaker.groovy
@@ -1,6 +1,5 @@
 package io.springframework.scstappstarters.common
 
-import groovy.transform.CompileStatic
 import io.springframework.common.Cron
 import io.springframework.common.JdkConfig
 import io.springframework.common.Maven
@@ -9,7 +8,6 @@ import javaposse.jobdsl.dsl.DslFactory
 
 import static io.springframework.common.Artifactory.artifactoryMaven3Configurator
 import static io.springframework.common.Artifactory.artifactoryMavenBuild
-
 /**
  * @author Marcin Grzejszczak
  */
@@ -26,6 +24,37 @@ class SpringScstAppStartersBuildMaker implements JdkConfig, TestPublisher,
         this.dsl = dsl
         this.organization = organization
         this.project = project
+    }
+
+    /**
+     * Dirty hack cause Jenkins is not inserting Maven to path...
+     * Requires using Maven3 installation before calling
+     *
+     * TODO: This doesn't belong here
+     */
+    String mavenBin() {
+        return "/opt/jenkins/data/tools/hudson.tasks.Maven_MavenInstallation/maven33/apache-maven-3.3.9/bin/"
+    }
+
+    /**
+     * TODO: This doesn't belong here
+     */
+    String setupGitCredentials() {
+        return """
+					set +x
+					git config user.name "${githubUserName()}"
+					git config user.email "${githubEmail()}"
+					git config credential.helper "store --file=/tmp/gitcredentials"
+					echo "https://\$${githubRepoUserNameEnvVar()}:\$${githubRepoPasswordEnvVar()}@github.com" > /tmp/gitcredentials
+					set -x
+				"""
+    }
+
+    /**
+     * TODO: This doesn't belong here
+     */
+    String cleanGitCredentials() {
+        return "rm -rf /tmp/gitcredentials"
     }
 
     void deploy() {
@@ -47,6 +76,15 @@ class SpringScstAppStartersBuildMaker implements JdkConfig, TestPublisher,
                     mavenInstallation(maven32())
                     goals('install -U -DskipTests')
                 }
+                shell("""#!/bin/bash -x
+					export MAVEN_PATH=${mavenBin()}
+					${setupGitCredentials()}
+					echo "Generating and build apps"
+					./generateApps.sh
+                    cd apps
+                    mvn clean install
+					${cleanGitCredentials()}
+					""")
             }
             configure {
                 artifactoryMavenBuild(it as Node) {
