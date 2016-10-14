0b031b302e78cf8096bb1505dcf5a345b9abb147
Soby Chacko <schacko@pivotal.io>
2016-10-14 18:01:25 -0400

    Debugging kafka binder CI tests failures

--- jobs/springcloudstream/spring_cloud_stream.groovy
+++ jobs/springcloudstream/spring_cloud_stream.groovy
@@ -7,7 +7,7 @@ DslFactory dsl = this
 
 // CI
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream").deploy()
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka").deploy()
+new SpringCloudStreamBuildMarker(dsl, "sobychacko", "spring-cloud-stream-binder-kafka").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-google-pubsub").deploy()
 
