e4510371f9d82a8eb4765821bfe4d0d8d415a154
Soby Chacko <schacko@pivotal.io>
2016-10-17 12:24:33 -0400

    test debugging scst

--- jobs/springcloudstream/spring_cloud_stream.groovy
+++ jobs/springcloudstream/spring_cloud_stream.groovy
@@ -6,7 +6,7 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory dsl = this
 
 // CI
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream").deploy()
+new SpringCloudStreamBuildMarker(dsl, "sobychacko", "spring-cloud-stream").deploy()
 new SpringCloudStreamBuildMarker(dsl, "sobychacko", "spring-cloud-stream-binder-kafka").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-google-pubsub").deploy()
