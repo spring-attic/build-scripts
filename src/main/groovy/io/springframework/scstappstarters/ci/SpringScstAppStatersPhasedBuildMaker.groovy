53050279d0b58ebea05fcb60d123aadb8031e973
Soby Chacko <schacko@pivotal.io>
2016-10-25 19:29:32 -0400

    refactoring around scst app starter build phases - separating core build

--- src/main/groovy/io/springframework/scstappstarters/ci/SpringScstAppStatersPhasedBuildMaker.groovy
+++ src/main/groovy/io/springframework/scstappstarters/ci/SpringScstAppStatersPhasedBuildMaker.groovy
@@ -3,6 +3,7 @@ package io.springframework.scstappstarters.ci
 import io.springframework.scstappstarters.common.AllScstAppStarterJobs
 import io.springframework.scstappstarters.common.SpringScstAppStarterJobs
 import javaposse.jobdsl.dsl.DslFactory
+
 /**
  * @author Soby Chacko
  */
@@ -11,7 +12,6 @@ class SpringScstAppStatersPhasedBuildMaker implements SpringScstAppStarterJobs {
     private final DslFactory dsl
 
     final String branchToBuild = "master"
-    final String coreProject = "core"
 
     SpringScstAppStatersPhasedBuildMaker(DslFactory dsl) {
         this.dsl = dsl
@@ -21,57 +21,38 @@ class SpringScstAppStatersPhasedBuildMaker implements SpringScstAppStarterJobs {
         buildAllRelatedJobs()
         dsl.multiJob("spring-scst-app-starter-builds") {
             steps {
+
+                phase('core-phase') {
+                    triggers {
+                        githubPush()
+                    }
+                    scm {
+                        git {
+                            remote {
+                                url "https://github.com/spring-cloud-stream-app-starters/core"
+                                branch branchToBuild
+                            }
+                        }
+                    }
+                    String prefixedProjectName = prefixJob("core")
+                    phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
+                        currentJobParameters()
+                    }
+                }
+
                 int counter = 1
                 (AllScstAppStarterJobs.PHASES).each { List<String> ph ->
                     phase("phase-${counter}-job") {
                         ph.each {
                             String projectName ->
-                            String prefixedProjectName = prefixJob(projectName)
-                            phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
-                                currentJobParameters()
-                            }
+                                String prefixedProjectName = prefixJob(projectName)
+                                phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
+                                    currentJobParameters()
+                                }
                         }
                     }
                     counter++;
                 }
-
-
-
-//                phase('phase-1-jobs') {
-//                    triggers {
-//                        githubPush()
-//                    }
-//                    scm {
-//                        git {
-//                            remote {
-//                                url "https://github.com/spring-cloud-stream-app-starters/core"
-//                                branch branchToBuild
-//                            }
-//                        }
-//                    }
-//                    (AllScstAppStarterJobs.CORE_PHASE).each { String projectName ->
-//                        String prefixedProjectName = prefixJob(projectName)
-//                        phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
-//                            currentJobParameters()
-//                        }
-//                    }
-//                }
-//                phase('phase-2-jobs') {
-//                    (AllScstAppStarterJobs.PHASE1_JOBS).each { String projectName ->
-//                        String prefixedProjectName = prefixJob(projectName)
-//                        phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
-//                            currentJobParameters()
-//                        }
-//                    }
-//                }
-//                phase('phase-3-jobs') {
-//                    (AllScstAppStarterJobs.PHASE2_JOBS).each { String projectName ->
-//                        String prefixedProjectName = prefixJob(projectName)
-//                        phaseJob("${prefixedProjectName}-${branchToBuild}-ci".toString()) {
-//                            currentJobParameters()
-//                        }
-//                    }
-//                }
             }
         }
     }
--- src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -8,7 +8,6 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class AllScstAppStarterJobs {
 
-    public static final List<String> CORE_PHASE = ['core']
     /**
      * List of all Spring Cloud jobs. This list will be used to create the boot compatibility builds
      * and will serve as basis for the default jobs
@@ -23,6 +22,6 @@ class AllScstAppStarterJobs {
 
     public static final List<String> ALL_JOBS = PHASE1_JOBS + PHASE2_JOBS
 
-    public static final List<List<String>> PHASES = [CORE_PHASE, PHASE1_JOBS, PHASE2_JOBS]
+    public static final List<List<String>> PHASES = [PHASE1_JOBS, PHASE2_JOBS]
 
 }
