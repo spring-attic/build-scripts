21aba6d2e1ac64b8d5a47220c891b2ab19f3e85d
Soby Chacko <schacko@pivotal.io>
2016-10-26 00:34:02 -0400

    Add http to the build for scst apps

--- src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -18,7 +18,7 @@ class AllScstAppStarterJobs {
      * List of all Spring Cloud jobs. This list will be used to create the boot compatibility builds
      * and will serve as basis for the default jobs
      */
-    public static final List<String> PHASE2_JOBS = ['file']
+    public static final List<String> PHASE2_JOBS = ['file', 'http']
 
     public static final List<String> ALL_JOBS = PHASE1_JOBS + PHASE2_JOBS
 
