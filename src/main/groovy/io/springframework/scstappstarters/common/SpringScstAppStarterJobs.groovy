6a3fa9dceb54fe63be794ef72bed2492b1e5189e
Soby Chacko <schacko@pivotal.io>
2016-11-08 16:13:06 -0500

    scst apps builds - fixing typo

--- src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStarterJobs.groovy
@@ -87,14 +87,14 @@ trait SpringScstAppStarterJobs extends BuildAndDeploy {
 					#!/bin/bash -x
 					git checkout master
 					./mvnw versions:set -DnewVersion=1.1.0.RC1 -DgenerateBackupPoms=false
-					./mvnw versions:set -DnewVersion=1.1.0.RC1 -DgenerateBackupPoms=false -pl :$project+"-app-dependencies"
+					./mvnw versions:set -DnewVersion=1.1.0.RC1 -DgenerateBackupPoms=false -pl :$project"-app-dependencies"
 					./mvnw versions:update-parent -DparentVersion=1.1.0.RC1 -Pspring -DgenerateBackupPoms=false
 			   		lines=\$(find . -type f -name pom.xml | xargs grep SNAPSHOT | wc -l)
 					if [ \$lines -eq 0 ]; then
 						./mvnw clean deploy -U -Pspring -PgenerateApps
 						git commit -am"Updating release version to 1.1.0.RC1"
 						./mvnw versions:set -DnewVersion=1.1.0.BUILD-SNAPSHOT -DgenerateBackupPoms=false
-						./mvnw versions:set -DnewVersion=1.1.0.BUILD-SNAPSHOT -DgenerateBackupPoms=false -pl :$project+"-app-dependencies"
+						./mvnw versions:set -DnewVersion=1.1.0.BUILD-SNAPSHOT -DgenerateBackupPoms=false -pl :$project"-app-dependencies"
 						./mvnw versions:update-parent -DparentVersion=1.1.0.BUILD-SNAPSHOT -Pspring -DgenerateBackupPoms=false
 						git commit -am"Updating next version to 1.1.0.BUILD-SNAPSHOT"
 						git push origin master
