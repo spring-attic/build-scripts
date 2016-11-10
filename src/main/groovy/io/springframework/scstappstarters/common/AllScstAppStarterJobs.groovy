c70088d655f4bfa4b8082b44f4178161ff753815
Soby Chacko <schacko@pivotal.io>
2016-11-10 16:43:33 -0500

    Reinstating all the app starter builds for scst

--- src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -8,35 +8,33 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class AllScstAppStarterJobs {
 
-    //public static final List<String> PHASE1_JOBS = ['log', 'time', 'ftp', 'sftp', 'file']
-    public static final List<String> PHASE1_JOBS = ['time', 'ftp', 'tcp', 'redis-pubsub']
+    public static final List<String> PHASE1_JOBS = ['log', 'time', 'ftp', 'sftp', 'file']
 
-//    public static final List<String> PHASE2_JOBS = ['jms', 'http', 'jdbc', 'twitter', 'rabbit']
-//
-//    public static final List<String> PHASE3_JOBS = ['gemfire', 'cassandra', 'hdfs', 'websocket', 'router']
-//
-//    public static final List<String> PHASE4_JOBS = ['counter', 'aggregate-counter', 'field-value-counter', 'bridge', 'gpfdist']
-//
-//    public static final List<String> PHASE5_JOBS = ['tasklauncher-local', 'tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'throughput', 'aws-s3']
-//
-//    public static final List<String> PHASE6_JOBS = ['filter', 'groovy-filter', 'groovy-transform', 'httpclient']
-//
-//    public static final List<String> PHASE7_JOBS = ['load-generator', 'loggregator', 'mail', 'mongodb', 'pmml']
-//
-//    public static final List<String> PHASE8_JOBS = ['redis-pubsub', 'scriptable-transform', 'splitter', 'syslog', 'tcp']
-//
-//    public static final List<String> PHASE9_JOBS = ['trigger', 'triggertask', 'tasklaunchrequest-transform', 'transform']
+    public static final List<String> PHASE2_JOBS = ['jms', 'http', 'jdbc', 'twitter', 'rabbit']
 
-    public static final List<String> ALL_JOBS = PHASE1_JOBS;
-//    + PHASE2_JOBS +
-//                                                PHASE3_JOBS + PHASE4_JOBS +
-//                                                PHASE5_JOBS + PHASE6_JOBS +
-//                                                PHASE7_JOBS + PHASE8_JOBS + PHASE9_JOBS;
+    public static final List<String> PHASE3_JOBS = ['gemfire', 'cassandra', 'hdfs', 'websocket', 'router']
 
-    public static final List<List<String>> PHASES = [PHASE1_JOBS]
-//                                                     , PHASE2_JOBS,
-//                                                     PHASE3_JOBS, PHASE4_JOBS,
-//                                                     PHASE5_JOBS, PHASE6_JOBS,
-//                                                     PHASE7_JOBS, PHASE8_JOBS, PHASE9_JOBS]
+    public static final List<String> PHASE4_JOBS = ['counter', 'aggregate-counter', 'field-value-counter', 'bridge', 'gpfdist']
+
+    public static final List<String> PHASE5_JOBS = ['tasklauncher-local', 'tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'throughput', 'aws-s3']
+
+    public static final List<String> PHASE6_JOBS = ['filter', 'groovy-filter', 'groovy-transform', 'httpclient']
+
+    public static final List<String> PHASE7_JOBS = ['load-generator', 'loggregator', 'mail', 'mongodb', 'pmml']
+
+    public static final List<String> PHASE8_JOBS = ['redis-pubsub', 'scriptable-transform', 'splitter', 'syslog', 'tcp']
+
+    public static final List<String> PHASE9_JOBS = ['trigger', 'triggertask', 'tasklaunchrequest-transform', 'transform']
+
+    public static final List<String> ALL_JOBS = PHASE1_JOBS + PHASE2_JOBS +
+                                                PHASE3_JOBS + PHASE4_JOBS +
+                                                PHASE5_JOBS + PHASE6_JOBS +
+                                                PHASE7_JOBS + PHASE8_JOBS + PHASE9_JOBS;
+
+    public static final List<List<String>> PHASES = [PHASE1_JOBS,
+                                                     PHASE2_JOBS,
+                                                     PHASE3_JOBS, PHASE4_JOBS,
+                                                     PHASE5_JOBS, PHASE6_JOBS,
+                                                     PHASE7_JOBS, PHASE8_JOBS, PHASE9_JOBS]
 
 }
