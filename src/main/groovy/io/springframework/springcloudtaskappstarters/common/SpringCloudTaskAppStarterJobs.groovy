3de4a79a62227d5db7806f53e60181d1962fd582
Soby Chacko <schacko@pivotal.io>
2016-11-23 16:47:55 -0500

    spring cloud task appstarters: release changes

--- src/main/groovy/io/springframework/springcloudtaskappstarters/common/SpringCloudTaskAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/springcloudtaskappstarters/common/SpringCloudTaskAppStarterJobs.groovy
@@ -11,4 +11,37 @@ trait SpringCloudTaskAppStarterJobs extends SpringScstAppStarterJobs {
     String projectSuffix() {
         return 'spring-cloud-task-app-starters'
     }
+
+    @Override
+    String cleanAndDeploy(String releaseVersion) {
+        return """
+					#!/bin/bash -x
+					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false
+					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false -pl :task-app-starters-core-dependencies
+			   		lines=\$(find . -type f -name pom.xml | xargs grep SNAPSHOT | wc -l)
+					if [ \$lines -eq 0 ]; then
+						./mvnw clean deploy -U -Pspring
+					else
+						echo "Snapshots found. Aborting the release build."
+					fi
+			   """
+
+    }
+
+    @Override
+    String cleanAndDeployWithGenerateApps(String project, String releaseVersion, String parentVersion) {
+        return """
+					#!/bin/bash -x
+					rm -rf apps
+					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false
+					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false -pl :$project"-task-app-dependencies"
+					./mvnw versions:update-parent -DparentVersion=$parentVersion -Pspring -DgenerateBackupPoms=false
+					lines=\$(find . -type f -name pom.xml | xargs grep SNAPSHOT | wc -l)
+					if [ \$lines -eq 0 ]; then
+						./mvnw clean deploy -U -Pspring -PgenerateApps
+					else
+						echo "Snapshots found. Aborting the release build."
+					fi
+			   """
+    }
 }
