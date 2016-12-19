0d5b29f72eac6ff50617d66fb4119e939f407618
Marius Bogoevici <mbogoevici@pivotal.io>
2016-12-19 09:16:54 -0500

    Add 1.1.x branch for Stream Kafka and Rabbit, refactor

--- jobs/springcloudstream/spring_cloud_stream.groovy
+++ jobs/springcloudstream/spring_cloud_stream.groovy
@@ -6,19 +6,34 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory dsl = this
 
 // CI
+
+// Spring Cloud Stream core builds
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream").deploy()
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy()
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit", [:]).deploy(true, false,
-        "clean deploy -U", "ci-docker-compose", "docker-compose-RABBITMQ.sh",
-        "docker-compose-RABBITMQ-stop.sh")
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-google-pubsub").deploy()
-
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters").deploy(false, true, "clean deploy -Pfull")
-new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters"
-    ,"Brooklyn.x").deploy(false, true, "clean deploy -Pfull")
-
-
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
         "1.1.x").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
         "1.0.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy(true, false, "clean deploy -Pfull")
+
+// Kafka binder builds
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy()
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka",
+        "1.1.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy()
+
+
+// Rabbit binder builds
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit", [:]).deploy(true, false,
+        "clean deploy -U", "ci-docker-compose", "docker-compose-RABBITMQ.sh",
+        "docker-compose-RABBITMQ-stop.sh")
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit",
+        "1.1.x", [:]).deploy(true, false,
+        "clean deploy -U", "ci-docker-compose", "docker-compose-RABBITMQ.sh",
+        "docker-compose-RABBITMQ-stop.sh")
+
+
+// Google PubSub binder builds
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-google-pubsub").deploy()
+
+// Spring Cloud Stream Release Builds
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters").deploy(false, true, "clean deploy -Pfull")
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters"
+    ,"Brooklyn.x").deploy(false, true, "clean deploy -Pfull")
