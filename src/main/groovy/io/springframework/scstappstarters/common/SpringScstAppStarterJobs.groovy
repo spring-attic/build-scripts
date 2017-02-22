f9ea9dee8a6228c32e9edf2d387de8c2d9e518ec
Soby Chacko <schacko@pivotal.io>
2017-02-22 10:38:52 -0500

    Scst apps Bacon.M1 release

--- src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/scstappstarters/common/SpringScstAppStarterJobs.groovy
@@ -81,7 +81,7 @@ trait SpringScstAppStarterJobs extends BuildAndDeploy {
 					rm -rf apps
 					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false
 					./mvnw versions:set -DnewVersion=$releaseVersion -DgenerateBackupPoms=false -pl :$project"-app-dependencies"
-					./mvnw versions:update-parent -DparentVersion=$parentVersion -Pspring -DgenerateBackupPoms=false
+					./mvnw versions:update-parent -DparentVersion=$parentVersion -Pspring -DgenerateBackupPoms=false -pl \\!$project-app-dependencies
 					lines=\$(find . -type f -name pom.xml | xargs grep SNAPSHOT | wc -l)
 					if [ \$lines -eq 0 ]; then
 						./mvnw clean deploy -U -Pspring -PgenerateApps
