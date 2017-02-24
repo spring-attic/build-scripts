e2a2ca151cb19e0ac6feb2235eca7609c09b83ad
Soby Chacko <schacko@pivotal.io>
2017-02-23 19:59:19 -0500

    spring cloud task apps build: disabling sqoop-tool build

--- src/main/groovy/io/springframework/springcloudtaskappstarters/common/AllSpringCloudTaskAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/springcloudtaskappstarters/common/AllSpringCloudTaskAppStarterJobs.groovy
@@ -9,8 +9,8 @@ import groovy.transform.CompileStatic
 class AllSpringCloudTaskAppStarterJobs {
 
     public static final List<String> PHASE1_JOBS = ['timestamp', 'spark-client', 'spark-cluster', 'spark-yarn']
-
-    public static final List<String> PHASE2_JOBS = ['sqoop-job', 'sqoop-tool', 'jdbchdfs-local']
+    //TODO: sqoop-tool is disabled due to test failures
+    public static final List<String> PHASE2_JOBS = ['sqoop-job', 'jdbchdfs-local']
 
     public static final List<String> ALL_JOBS = PHASE1_JOBS + PHASE2_JOBS;
 
@@ -18,8 +18,8 @@ class AllSpringCloudTaskAppStarterJobs {
                                                      PHASE2_JOBS]
 
     public static final Map<String, String> RELEASE_PHASE1_JOBS = ['timestamp':'1.2.0.M1', 'spark-client':'1.2.0.M1', 'spark-cluster':'1.2.0.M1', 'spark-yarn':'1.2.0.M1']
-
-    public static final Map<String, String> RELEASE_PHASE2_JOBS = ['sqoop-job':'1.2.0.M1', 'sqoop-tool':'1.2.0.M1', 'jdbchdfs-local':'1.2.0.M1']
+    //TODO: Add sqoop-tool back
+    public static final Map<String, String> RELEASE_PHASE2_JOBS = ['sqoop-job':'1.2.0.M1', 'jdbchdfs-local':'1.2.0.M1']
 
 
     public static final Map<String, String> RELEASE_ALL_JOBS =
