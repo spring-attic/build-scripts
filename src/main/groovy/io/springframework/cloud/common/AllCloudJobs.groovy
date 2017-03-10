8e7f5a7a0cb17899de53ddb05f6052ba1892b047
Ryan Baxter <rbaxter85@gmail.com>
2017-03-10 15:59:22 -0500

    Reverting Consul branch job

--- src/main/groovy/io/springframework/cloud/common/AllCloudJobs.groovy
+++ src/main/groovy/io/springframework/cloud/common/AllCloudJobs.groovy
@@ -60,8 +60,7 @@ class AllCloudJobs {
 																		'spring-cloud-commons' : ['1.0.x', '1.1.x'],
 																		'spring-cloud-config' : ['1.1.x', '1.2.x'],
 																		'spring-cloud-zookeeper' : ['1.0.x'],
-																		'spring-cloud-bus': ['1.2.x'],
-									                                                                        'spring-cloud-consul' : ['1.1.x']]
+																		'spring-cloud-bus': ['1.2.x']]
 
 	/**
 	 * List of default jobs. Default means that `./mvnw clean deploy` will be executed to publish artifacts
