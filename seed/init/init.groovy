53b5f5ac23f63e3ac5c1899adc3a20f575fa6888
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-04-10 14:56:45 +0200

    Updated init.groovy

--- seed/init/init.groovy
+++ seed/init/init.groovy
@@ -5,7 +5,7 @@ def seeds = new File('/usr/share/jenkins/seeds/')
 def jobManagement = new JenkinsJobManagement(System.out, [:], new File('.'))
 
 new DslScriptLoader(jobManagement).with {
-	seeds.eachFileRecurse (FileType.FILES) { file ->
+	seeds.eachFileRecurse (groovy.io.FileType.FILES) { file ->
   		runScript(file.text)
 	}
 }
