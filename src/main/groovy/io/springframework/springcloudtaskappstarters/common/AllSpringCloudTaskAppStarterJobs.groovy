243112f561e2f6e988f10a778ad4d463fa926922
Glenn Renfro <grenfro@pivotal.io>
2017-04-10 17:25:23 -0400

    Added composed-task-runner to the AllSpringCloudTaskAppStarterJobs
    Also added it to RELEASE_PHASE1_JOBS.  Please verify that is correct.

--- src/main/groovy/io/springframework/springcloudtaskappstarters/common/AllSpringCloudTaskAppStarterJobs.groovy
+++ src/main/groovy/io/springframework/springcloudtaskappstarters/common/AllSpringCloudTaskAppStarterJobs.groovy
@@ -8,7 +8,7 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class AllSpringCloudTaskAppStarterJobs {
 
-    public static final List<String> PHASE1_JOBS = ['timestamp', 'spark-client', 'spark-cluster', 'spark-yarn']
+    public static final List<String> PHASE1_JOBS = ['timestamp', 'spark-client', 'spark-cluster', 'spark-yarn', 'composed-task-runner']
     //TODO: sqoop-tool is disabled due to test failures
     public static final List<String> PHASE2_JOBS = ['sqoop-job', 'jdbchdfs-local']
 
@@ -17,7 +17,7 @@ class AllSpringCloudTaskAppStarterJobs {
     public static final List<List<String>> PHASES = [PHASE1_JOBS,
                                                      PHASE2_JOBS]
 
-    public static final Map<String, String> RELEASE_PHASE1_JOBS = ['timestamp':'1.2.0.M1', 'spark-client':'1.2.0.M1', 'spark-cluster':'1.2.0.M1', 'spark-yarn':'1.2.0.M1']
+    public static final Map<String, String> RELEASE_PHASE1_JOBS = ['timestamp':'1.2.0.M1', 'spark-client':'1.2.0.M1', 'spark-cluster':'1.2.0.M1', 'spark-yarn':'1.2.0.M1', 'composed-task-runner':'1.2.0.M1']
     //TODO: Add sqoop-tool back
     public static final Map<String, String> RELEASE_PHASE2_JOBS = ['sqoop-job':'1.2.0.M1', 'jdbchdfs-local':'1.2.0.M1']
 
