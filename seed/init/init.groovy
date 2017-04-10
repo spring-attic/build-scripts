cbe02a0f154c2502886681b0bf2804575ee03d3c
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-04-10 15:25:47 +0200

    Updated init.groovy

--- seed/init/init.groovy
+++ seed/init/init.groovy
@@ -12,6 +12,7 @@ new DslScriptLoader(jobManagement).with {
 
 println "Downloading the custom JMH Plugin"
 def url = 'https://bintray.com/marcingrzejszczak/jenkins/download_file?file_path=jmh-jenkins%2F0.0.1%2Fjmhbenchmark.hpi'
-def file = new File("${System.getenv('JENKINS_HOME')}/plugins").newOutputStream()
-file << new URL(url).openStream()
-file.close()
+def file = new File("${System.getenv('JENKINS_HOME')}/plugins/jmhbenchmark.hpi")
+def stream = file.newOutputStream()
+stream << new URL(url).openStream()
+stream.close()
