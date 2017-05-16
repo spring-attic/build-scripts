6e5a71954d75797546bb24f46c2649cb66362130
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-05-16 12:36:58 +0200

    Updated dashboard

--- src/main/groovy/io/springframework/common/view/Dashboard.groovy
+++ src/main/groovy/io/springframework/common/view/Dashboard.groovy
@@ -6,7 +6,7 @@ import javaposse.jobdsl.dsl.View
 class Dashboard extends View {
 
 	protected Dashboard(JobManagement jobManagement) {
-		super(jobManagement)
+		super(jobManagement, "dashboard")
 	}
 
 	@Override
