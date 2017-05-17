08e8dc159fc84e7bd1ea09d29b907ec74623c3cc
Soby Chacko <schacko@pivotal.io>
2017-05-17 14:37:05 -0400

    Adding builds for scst 1.2.x/Chelsea.x branches

--- jobs/springcloudstream/spring_cloud_stream.groovy
+++ jobs/springcloudstream/spring_cloud_stream.groovy
@@ -6,21 +6,26 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dsl = this
 
-// CI
-
+// Master builds
 new SpringCloudStreamPhasedBuildMaker(dsl).build(false)
 
-// Spring Cloud Stream core builds (non master)
+// Spring Cloud Stream core builds (1.2.x)
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
+        "1.2.x").deploy()
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka",
+        "1.2.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy()
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit",
+        "1.2.x", [:]).deploy(true, false,
+        "clean deploy -U -Pspring", "ci-docker-compose", "docker-compose-RABBITMQ.sh",
+        "docker-compose-RABBITMQ-stop.sh")
+
+// Spring Cloud Stream core builds (1.1.x/1.0.x)
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
         "1.1.x").deploy()
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
         "1.0.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy(true, false, "clean deploy -Pfull,spring")
-
-// Kafka binder builds (non master)
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka",
         "1.1.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy()
-
-// Rabbit binder builds (non master)
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-rabbit",
         "1.1.x", [:]).deploy(true, false,
         "clean deploy -U -Pspring", "ci-docker-compose", "docker-compose-RABBITMQ.sh",
@@ -31,3 +36,4 @@ new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binde
 
 // Spring Cloud Stream Release Builds (non master)
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters", "Brooklyn.x").deploy(false, true, "clean package -Pspring", null, null, null, true)
+new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-starters", "Chelsea.x").deploy(false, true, "clean package -Pspring", null, null, null, true)
