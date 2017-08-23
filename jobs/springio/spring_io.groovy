6d9e32f3af2a48326d3b95a724d999dd2647ea5f
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-08-23 12:47:44 +0200

    Added missing rollback build maker

--- jobs/springio/spring_io.groovy
+++ jobs/springio/spring_io.groovy
@@ -4,9 +4,12 @@ import org.springframework.jenkins.springio.ci.SpringInitializrBuildMaker
 import org.springframework.jenkins.springio.ci.SpringStarterProductionBuildMaker
 import javaposse.jobdsl.dsl.DslFactory
 
+import org.springframework.jenkins.springio.ci.SpringStarterRollbackBuildMaker
+
 DslFactory dsl = this
 String scriptsDir = binding.variables["SCRIPTS_DIR"] ?: "${WORKSPACE}/src/main/bash/springio"
 
 // CI
 new SpringInitializrBuildMaker(dsl).build()
 new SpringStarterProductionBuildMaker(dsl, scriptsDir).deploy()
+new SpringStarterRollbackBuildMaker(dsl, scriptsDir).deploy()
