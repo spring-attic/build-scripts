212a63c182f83d72b6d673cc663f24dfc0ff0700
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-08-23 15:05:21 +0200

    Adding a default value of domain name

--- seed/projects/spring_io_seed.groovy
+++ seed/projects/spring_io_seed.groovy
@@ -12,11 +12,11 @@ job('spring-io-seed') {
     }
     wrappers {
         parameters {
-            stringParam('DOMAIN_NAME', '', 'Domain of the deployed application [REQUIRED PARAMETER]')
             stringParam('BLUE_APP_NAME', 'start-blue', 'The name of the blue instance')
             stringParam('GREEN_APP_NAME', 'start-green', 'The name of the green instance')
             stringParam('BLUE_APP_HOSTNAME', 'start-staging-blue', 'The hostname of the blue instance')
             stringParam('GREEN_APP_HOSTNAME', 'start-staging-green', 'The hostname of the green instance')
+            stringParam('DOMAIN_NAME', 'spring.io', 'Domain of the deployed application')
             stringParam('ROUTED_HOSTNAME', 'start-staging', 'The hostname to which the "production" traffic gets routed')
             stringParam('JAR_LOCATION', 'initializr-service/target/initializr-service.jar', 'Location of the JAR to be deployed')
             stringParam('OLD_APP_INSTANCES', '1', 'Number of instances of the old instance. If you pass [0] then the old instance will get stopped')
