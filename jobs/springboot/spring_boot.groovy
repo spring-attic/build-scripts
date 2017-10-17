1375079a619e172fe53734b353d83bda990cb9ae
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-10-17 15:38:50 +0200

    Disabling Boot

--- jobs/springboot/spring_boot.groovy
+++ jobs/springboot/spring_boot.groovy
@@ -8,6 +8,6 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory dsl = this
 
 // CI
-new SpringBootDeployBuildMaker(dsl).deploy()
-new SpringBootWindowsBuildMaker(dsl).deploy()
-new SpringBootIntegrationBuildMaker(dsl).deploy()
+// new SpringBootDeployBuildMaker(dsl).deploy()
+// new SpringBootWindowsBuildMaker(dsl).deploy()
+// new SpringBootIntegrationBuildMaker(dsl).deploy()
