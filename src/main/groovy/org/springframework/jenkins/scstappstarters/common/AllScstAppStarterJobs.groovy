2b6044df3402f18cf20863b9b9677f557c9cf55f
Soby Chacko <schacko@pivotal.io>
2017-11-27 15:18:03 -0500

    scst apps build - making changes in job phases

--- src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -8,7 +8,9 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class AllScstAppStarterJobs {
 
-    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local', 'header-enricher', 'function']
+    public static final List<String> PHASE0_JOBS = ['log', 'time']
+
+    public static final List<String> PHASE1_JOBS = ['throughput', 'bridge', 'groovy-transform', 'tasklauncher-local', 'header-enricher', 'function']
 
     public static final List<String> PHASE2_JOBS = ['router', 'trigger', 'loggregator', 'load-generator', 'mail', 'field-value-counter', 'pmml', 'tasklaunchrequest-transform']
 
@@ -21,18 +23,18 @@ class AllScstAppStarterJobs {
     public static final List<String> PHASE6_JOBS = ['sftp', 'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'gemfire', 'tcp', 'transform', 'python']
 
     public static final List<String> ALL_JOBS =
-            PHASE1_JOBS + PHASE2_JOBS +
+            PHASE0_JOBS + PHASE1_JOBS + PHASE2_JOBS +
                     PHASE3_JOBS + PHASE4_JOBS +
                     PHASE5_JOBS + PHASE6_JOBS
 
     public static final List<List<String>> PHASES = [
+            PHASE0_JOBS,
             PHASE1_JOBS, PHASE2_JOBS,
             PHASE3_JOBS, PHASE4_JOBS,
             PHASE5_JOBS, PHASE6_JOBS]
 
-    public static final Map<String, String> RELEASE_PHASE0_JOBS = ['log':'1.3.0.M1', 'time':'1.3.0.M1']
 
-    public static final Map<String, String> RELEASE_PHASE1_JOBS = ['throughput':'1.3.0.M1', 'bridge':'1.3.0.M1', 'groovy-transform':'1.3.0.M1', 'tasklauncher-local':'1.3.0.M1', 'transform':'1.3.0.M1', 'header-enricher':'1.3.0.M1']
+    public static final Map<String, String> RELEASE_PHASE1_JOBS = ['log':'1.3.0.M1', 'time':'1.3.0.M1', 'throughput':'1.3.0.M1', 'bridge':'1.3.0.M1', 'groovy-transform':'1.3.0.M1', 'tasklauncher-local':'1.3.0.M1', 'transform':'1.3.0.M1', 'header-enricher':'1.3.0.M1']
 
     public static final Map<String, String> RELEASE_PHASE2_JOBS = ['router':'1.3.0.M1', 'trigger':'1.1.3.M1', 'loggregator':'1.3.0.M1', 'load-generator':'1.3.0.M1', 'mail':'1.3.0.M1', 'field-value-counter':'1.3.0.M1', 'pmml':'1.3.0.M1', 'tasklaunchrequest-transform':'1.3.0.M1']
 
@@ -45,7 +47,6 @@ class AllScstAppStarterJobs {
     public static final Map<String, String> RELEASE_PHASE6_JOBS = ['sftp':'1.3.0.M1', 'tensorflow':'1.3.0.M1', 'mongodb':'1.3.0.M1', 'hdfs':'1.3.0.M1', 'jdbc':'1.0.0.M1', 'gemfire':'1.3.0.M1','tcp':'1.3.0.M1','python':'1.3.0.M1']
 
     public static final Map<String, String> RELEASE_ALL_JOBS =
-            RELEASE_PHASE0_JOBS +
             RELEASE_PHASE1_JOBS + RELEASE_PHASE2_JOBS +
                     RELEASE_PHASE3_JOBS + RELEASE_PHASE4_JOBS +
                     RELEASE_PHASE5_JOBS + RELEASE_PHASE6_JOBS;
