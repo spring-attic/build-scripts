db2e5fec8236e3b5e6366e9336af2279bdc9dfcd
Soby Chacko <schacko@pivotal.io>
2017-11-28 19:50:20 -0500

    enabling snapshot builds in sctas builds

--- jobs/springcloudtaskappstarters/spring_cloud_task_app_starters.groovy
+++ jobs/springcloudtaskappstarters/spring_cloud_task_app_starters.groovy
@@ -5,7 +5,7 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dsl = this
 
-String releaseType = "ga" // possible values are - milestone or ga
+String releaseType = "" // possible values are - milestone or ga
 
 // CI builds for spring cloud task app starters
-new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(true, releaseType)
+new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(false, releaseType)
