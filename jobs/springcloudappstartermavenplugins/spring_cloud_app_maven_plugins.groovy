3e460aa2fd66d1faa577b5a655a4e918f747c90f
Soby Chacko <schacko@pivotal.io>
2018-01-31 10:15:39 -0500

    scst app starter changes for 1.3.x build

--- jobs/springcloudappstartermavenplugins/spring_cloud_app_maven_plugins.groovy
+++ jobs/springcloudappstartermavenplugins/spring_cloud_app_maven_plugins.groovy
@@ -8,5 +8,5 @@ DslFactory dsl = this
 // CI
 new SpringCloudAppStarterStarterMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-app-starters-maven-plugins", "spring-cloud-app-starter-doc-maven-plugin").deploy()
 new SpringCloudAppStarterStarterMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-app-starters-maven-plugins", "spring-cloud-app-starter-metadata-maven-plugin").deploy()
-new SpringCloudAppStarterStarterMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-app-starters-maven-plugins", "spring-cloud-stream-app-maven-plugin").deploy(false, true)
+new SpringCloudAppStarterStarterMavenPluginsBuildMaker(dsl, "spring-cloud", "spring-cloud-app-starters-maven-plugins", "spring-cloud-stream-app-maven-plugin").deploy(false, false)
 
