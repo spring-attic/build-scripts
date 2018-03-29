378d3c73029c4b813ee1eed50159c37f5691de3f
Soby Chacko <schacko@pivotal.io>
2018-03-29 12:59:27 -0400

    tasklaunder-k8s 1.0.0.M1 release

--- jobs/scstappstarters/spring_scst_app_starters.groovy
+++ jobs/scstappstarters/spring_scst_app_starters.groovy
@@ -5,10 +5,10 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dsl = this
 
-String releaseType = "" // possible values are - "", milestone or ga
+String releaseType = "milestone" // possible values are - "", milestone or ga
 
 // Master CI
-new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, releaseType)
+new SpringScstAppStatersPhasedBuildMaker(dsl).build(true, releaseType)
 
 // 1.3.x CI
 new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, "", "1.3.x")
