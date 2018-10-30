9e5beba8ee1bac1e16547cdb0efdbcc95655e808
Soby Chacko <schacko@pivotal.io>
2018-10-30 14:40:57 -0400

    Temporarily disable snapshot builds for scst apps.

--- jobs/scstappstarters/spring_scst_app_starters.groovy
+++ jobs/scstappstarters/spring_scst_app_starters.groovy
@@ -8,7 +8,7 @@ DslFactory dsl = this
 String releaseType = "" // possible values are - "", milestone or ga
 
 // Master CI
-new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, "")
+new SpringScstAppStatersPhasedBuildMaker(dsl).build(true, "milestone")
 
 // 1.3.x CI
 new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, "", "1.3.x")
