abe6d7bc75db673e00aab857557903cc7d383206
Soby Chacko <schacko@pivotal.io>
2018-12-13 17:09:28 -0500

    stream apps Einstein.M1

--- src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -10,30 +10,52 @@ class AllScstAppStarterJobs {
 
     //Removed cassandra and gpfdist from Darwin builds
 
+//    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local',
+//                                                    'header-enricher', 'router', 'tasklauncher-data-flow']
+//
+//    public static final List<String> PHASE2_JOBS = ['grpc', 'trigger', 'loggregator', 'load-generator', 'mail', 'field-value-counter',
+//                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform']
+//
+//    public static final List<String> PHASE3_JOBS = ['websocket', 'http', 'syslog', 'filter', 'splitter', 'jms', 'groovy-filter',
+//                                                    'redis-pubsub', 'aggregate-counter', 'tcp']
+//
+//    public static final List<String> PHASE4_JOBS = ['tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'tasklauncher-kubernetes',
+//                                                    'httpclient', 'twitter', 'counter', 'aggregator', 'mqtt', 'rabbit', 'gemfire']
+//
+//    public static final List<String> PHASE5_JOBS = ['scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
+//                                                    'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'python']
+//
+//    public static final List<String> ALL_JOBS =
+//            PHASE1_JOBS + PHASE2_JOBS +
+//                    PHASE3_JOBS + PHASE4_JOBS +
+//                    PHASE5_JOBS
+//
+//    public static final List<List<String>> PHASES = [
+//            PHASE1_JOBS, PHASE2_JOBS,
+//            PHASE3_JOBS, PHASE4_JOBS,
+//            PHASE5_JOBS]
+
     public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local',
-                                                    'header-enricher', 'router', 'tasklauncher-data-flow']
+                                                    'header-enricher', 'router', 'tasklauncher-data-flow', 'grpc', 'trigger', 'loggregator']
 
-    public static final List<String> PHASE2_JOBS = ['grpc', 'trigger', 'loggregator', 'load-generator', 'mail', 'field-value-counter',
-                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform']
+    public static final List<String> PHASE2_JOBS = ['load-generator', 'mail', 'field-value-counter',
+                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform', 'websocket', 'http', 'syslog', 'filter', 'splitter']
 
-    public static final List<String> PHASE3_JOBS = ['websocket', 'http', 'syslog', 'filter', 'splitter', 'jms', 'groovy-filter',
-                                                    'redis-pubsub', 'aggregate-counter', 'tcp']
+    public static final List<String> PHASE3_JOBS = ['jms', 'groovy-filter',
+                                                    'redis-pubsub', 'aggregate-counter', 'tcp',
+                                                    'tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'tasklauncher-kubernetes',
+                                                    'httpclient', 'twitter', 'counter', 'aggregator']
 
-    public static final List<String> PHASE4_JOBS = ['tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'tasklauncher-kubernetes',
-                                                    'httpclient', 'twitter', 'counter', 'aggregator', 'mqtt', 'rabbit', 'gemfire']
-
-    public static final List<String> PHASE5_JOBS = ['scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
+    public static final List<String> PHASE4_JOBS = ['mqtt', 'rabbit', 'gemfire', 'scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
                                                     'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'python']
 
     public static final List<String> ALL_JOBS =
             PHASE1_JOBS + PHASE2_JOBS +
-                    PHASE3_JOBS + PHASE4_JOBS +
-                    PHASE5_JOBS
+                    PHASE3_JOBS + PHASE4_JOBS
 
     public static final List<List<String>> PHASES = [
             PHASE1_JOBS, PHASE2_JOBS,
-            PHASE3_JOBS, PHASE4_JOBS,
-            PHASE5_JOBS]
+            PHASE3_JOBS, PHASE4_JOBS]
 
     public static final List<String> DARWIN_PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local',
                                                     'header-enricher', 'router']
