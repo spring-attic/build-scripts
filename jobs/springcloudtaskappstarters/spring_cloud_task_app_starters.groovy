ffbf84a000f3457791c04e7662e9aa6076fc8c2f
Soby Chacko <schacko@pivotal.io>
2018-12-13 20:40:05 -0500

    task apps: Elston.M1 release

--- jobs/springcloudtaskappstarters/spring_cloud_task_app_starters.groovy
+++ jobs/springcloudtaskappstarters/spring_cloud_task_app_starters.groovy
@@ -17,7 +17,7 @@ String releaseType = "" // possible values are - milestone or ga
 //new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(true, "ga", "Clark")
 
 // master builds
-new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(false, "")
+new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(true, "milestone")
 
 // Dearborn builds
 new SpringCloudTaskAppStatersPhasedBuildMaker(dsl).build(false, "", "2.0.x")
