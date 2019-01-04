5473f6fb05b72d97b5e0267f6550907610e3a2a8
Soby Chacko <schacko@pivotal.io>
2019-01-04 17:24:58 -0500

    Einstein builds back to snapshots

--- src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
+++ src/main/groovy/org/springframework/jenkins/scstappstarters/common/AllScstAppStarterJobs.groovy
@@ -10,52 +10,51 @@ class AllScstAppStarterJobs {
 
     //Removed cassandra and gpfdist from Darwin builds
 
-//    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local',
-//                                                    'header-enricher', 'router', 'tasklauncher-data-flow']
+    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform',
+                                                    'header-enricher', 'router', 'tasklauncher-data-flow', 'grpc', 'trigger']
+
+    public static final List<String> PHASE2_JOBS = ['loggregator', 'load-generator', 'mail', 'field-value-counter',
+                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform', 'websocket', 'http']
+
+    public static final List<String> PHASE3_JOBS = ['syslog', 'filter', 'splitter', 'jms', 'groovy-filter',
+                                                    'redis-pubsub', 'aggregate-counter', 'tcp', 'httpclient']
+
+    public static final List<String> PHASE4_JOBS = ['twitter', 'counter', 'aggregator', 'mqtt', 'rabbit', 'gemfire',
+                                                    'scriptable-transform', 'ftp', 'file', ]
+
+    public static final List<String> PHASE5_JOBS = ['aws-s3', 'sftp', 'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'python']
+
+    public static final List<String> ALL_JOBS =
+            PHASE1_JOBS + PHASE2_JOBS +
+                    PHASE3_JOBS + PHASE4_JOBS +
+                    PHASE5_JOBS
+
+    public static final List<List<String>> PHASES = [
+            PHASE1_JOBS, PHASE2_JOBS,
+            PHASE3_JOBS, PHASE4_JOBS,
+            PHASE5_JOBS]
+
+    //Could be useful for the release as it reduces one phase
+//    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform',
+//                                                    'header-enricher', 'router', 'tasklauncher-data-flow', 'grpc', 'trigger', 'loggregator']
 //
-//    public static final List<String> PHASE2_JOBS = ['grpc', 'trigger', 'loggregator', 'load-generator', 'mail', 'field-value-counter',
-//                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform']
+//    public static final List<String> PHASE2_JOBS = ['load-generator', 'mail', 'field-value-counter',
+//                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform', 'websocket', 'http', 'syslog', 'filter', 'splitter']
 //
-//    public static final List<String> PHASE3_JOBS = ['websocket', 'http', 'syslog', 'filter', 'splitter', 'jms', 'groovy-filter',
-//                                                    'redis-pubsub', 'aggregate-counter', 'tcp']
+//    public static final List<String> PHASE3_JOBS = ['jms', 'groovy-filter',
+//                                                    'redis-pubsub', 'aggregate-counter', 'tcp',
+//                                                    'httpclient', 'twitter', 'counter', 'mqtt', 'aggregator']
 //
-//    public static final List<String> PHASE4_JOBS = ['tasklauncher-yarn', 'tasklauncher-cloudfoundry', 'tasklauncher-kubernetes',
-//                                                    'httpclient', 'twitter', 'counter', 'aggregator', 'mqtt', 'rabbit', 'gemfire']
-//
-//    public static final List<String> PHASE5_JOBS = ['scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
+//    public static final List<String> PHASE4_JOBS = ['rabbit', 'gemfire', 'scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
 //                                                    'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'python']
 //
 //    public static final List<String> ALL_JOBS =
 //            PHASE1_JOBS + PHASE2_JOBS +
-//                    PHASE3_JOBS + PHASE4_JOBS +
-//                    PHASE5_JOBS
+//                    PHASE3_JOBS + PHASE4_JOBS
 //
 //    public static final List<List<String>> PHASES = [
 //            PHASE1_JOBS, PHASE2_JOBS,
-//            PHASE3_JOBS, PHASE4_JOBS,
-//            PHASE5_JOBS]
-
-    //Could be useful for the release as it reduces one phase
-    public static final List<String> PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform',
-                                                    'header-enricher', 'router', 'tasklauncher-data-flow', 'grpc', 'trigger', 'loggregator']
-
-    public static final List<String> PHASE2_JOBS = ['load-generator', 'mail', 'field-value-counter',
-                                                    'pmml', 'tasklaunchrequest-transform', 'triggertask', 'transform', 'websocket', 'http', 'syslog', 'filter', 'splitter']
-
-    public static final List<String> PHASE3_JOBS = ['jms', 'groovy-filter',
-                                                    'redis-pubsub', 'aggregate-counter', 'tcp',
-                                                    'httpclient', 'twitter', 'counter', 'mqtt', 'aggregator']
-
-    public static final List<String> PHASE4_JOBS = ['rabbit', 'gemfire', 'scriptable-transform', 'ftp', 'file', 'aws-s3', 'sftp',
-                                                    'tensorflow', 'mongodb', 'hdfs', 'jdbc', 'python']
-
-    public static final List<String> ALL_JOBS =
-            PHASE1_JOBS + PHASE2_JOBS +
-                    PHASE3_JOBS + PHASE4_JOBS
-
-    public static final List<List<String>> PHASES = [
-            PHASE1_JOBS, PHASE2_JOBS,
-            PHASE3_JOBS, PHASE4_JOBS]
+//            PHASE3_JOBS, PHASE4_JOBS]
 
     public static final List<String> DARWIN_PHASE1_JOBS = ['log', 'time', 'throughput', 'bridge', 'groovy-transform', 'tasklauncher-local',
                                                     'header-enricher', 'router']
