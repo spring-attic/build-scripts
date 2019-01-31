06c717c5d1c46ca3f979a6a55503b2613d209e58
Soby Chacko <schacko@pivotal.io>
2019-01-31 17:41:41 -0500

    apps build back to snapshots

--- jobs/scstappstarters/spring_scst_app_starters.groovy
+++ jobs/scstappstarters/spring_scst_app_starters.groovy
@@ -8,7 +8,7 @@ DslFactory dsl = this
 String releaseType = "" // possible values are - "", milestone or ga
 
 // Master CI
-new SpringScstAppStatersPhasedBuildMaker(dsl).build(true, "milestone")
+new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, "")
 
 // 1.3.x CI
 new SpringScstAppStatersPhasedBuildMaker(dsl).build(false, "", "1.3.x")
