d1d71c285c666dbaa31bfc4bb417583b78f35518
Soby Chacko <schacko@pivotal.io>
2020-03-27 15:49:40 -0400

    metadata plugin build back to snapshot app plugin 1.2.13.RELEASE from for-einstein-elston

--- src/main/groovy/org/springframework/jenkins/springcloudappstartermavenplugins/ci/SpringCloudAppStarterStarterMavenPluginsBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudappstartermavenplugins/ci/SpringCloudAppStarterStarterMavenPluginsBuildMaker.groovy
@@ -18,7 +18,7 @@ class SpringCloudAppStarterStarterMavenPluginsBuildMaker implements JdkConfig, T
     final String repo
     final String project
 
-    String branchToBuild = "master"
+    String branchToBuild = "for-einstein-elston"
 
     Map<String, Object> envVariables = new HashMap<>();
 
